708d64eabd669070bd4a1992df832a418b59f2f5
natgitinit <natgitinit@gmail.com>
2023-11-02 12:53:41 -0500

    reversed integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//     //toString = turns number into string
+//     //split = turns string into array
+//     //reverse = reverses array 
+//     //join = creates new string and concatenates into array 
+//     // parseFloat = parses a string and returns a floating number again
+//     // Math.sign = returns whether a number is negative, positive or zero
+//     return (
+//         parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n)
+//     )
+// }
 
+function reverseInt(n) { 
+    const reversed = n.toString().split('').reverse().join('');
+
+    return parseInt(reversed) * Math.sign(n);
+            
+}
 module.exports = reverseInt;
