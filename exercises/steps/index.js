080d994a00003caa4d7b0f8f563b41e7f1ebae3a
natgitinit <natgitinit@gmail.com>
2023-11-05 11:16:16 -0600

    recursion function

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,50 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// for loop from 0 to n (through the rows)
+// create empty string to form 'stair'
+// for loop left to right (through the columns)
+// if current column is <= current row, add a '#' 
+// else add space to 'stair' 
+// ---------------------
+// |    #    |         | 
+// ---------------------
+
+// function steps(n) {
+    
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+        
+//         for( let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' '; 
+//             }
+//         }
+//         console.log(stair)
+//     }
+// }
+
+function steps(n, row = 0, stair = '') {
+    //base case 
+    if (n === row) {
+        return; 
+    }
+
+    //recursive call 
+    if ( n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    //second recursive call to add '#' if length is less than row
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
+}
 
 module.exports = steps;
