545e17515026abb446386d02f05bf416822fdaaa
natgitinit <natgitinit@gmail.com>
2023-11-17 12:46:41 -0600

    rotate matrix

--- exercises/rotateimage/index.js
+++ exercises/rotateimage/index.js
@@ -8,7 +8,7 @@ function rotateImage(matrix) {
     for(let i =0; i < n; i++) {
         for(let j= i; j < n; j++) {
             // swap matrix[i][j] with matrix[j][i]
-            // [[1234], [1234]] = [[4321], [4321]]
+            // [[1234], [4321]] = [[4321], [1234]]
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
         }
     }
